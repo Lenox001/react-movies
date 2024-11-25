a086b076b9763b14ae1bcdbdcf02a5008851b597
Larry Lenox <larrylenx9@gmail.com>
2024-11-25 12:10:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=ee7b1e07";
+const API_URL = "https://www.omdbapi.com/?apikey=ee7b1e07";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
